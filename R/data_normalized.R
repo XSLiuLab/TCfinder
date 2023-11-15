1f7445cf77483cef49e4749a2dc9b8234602d364
wuchx <chenxuwu1018@163.com>
2023-11-15 16:17:09 +0800

    update

--- R/data_normalized.R
+++ R/data_normalized.R
@@ -3,28 +3,37 @@
 #' @description Normalize single-cell raw counts matrix.
 #' @details Input a data.frame where the rows are the gene names and the columns are the sample names.
 #' @param expr_data A single-cell counts expression matrix.
-#' @param method Single cell sequencing type, "10X" or "smart-seq2"
-#' @param genome Reference genome, when method == "smart-seq2",
+#' @param method If the single-cell sequencing method used is smart-seq2, method = "smart-seq2" is required.
+#' For other single-cell sequencing methods, this parameter does not need to be filled in.
+#' @param genome Reference genome, when method = "smart-seq2",
 #' this parameter needs to be filled in, you can choose hg19 and hg38
 #' @return A normalized single-cell expression matrix.
 #' @export
-#' @importFrom dplyr %>% filter select mutate_all funs
+#' @importFrom Matrix Matrix Diagonal colSums
+#' @importFrom methods is
 
 
-data_normalized <- function(expr_data,method,genome = "hg38"){
+data_normalized <- function(expr_data,method = "method",genome = "hg38"){
 
 
-  if (method == "10X") {
+  if (!methods::is(expr_data, "CsparseMatrix")) {
 
-    gene_id <- rownames(expr_data)
+    expr_data <- Matrix::Matrix(as.matrix(expr_data),sparse = T)
 
-    data1 <- expr_data %>% apply(2,function(x){x/sum(x) * 10000}) %>% as.data.frame()
-    data2 <- data1 %>% dplyr::mutate_all(funs(log2(.+1)))
-
-    rownames(data2) <- gene_id
-    data2 <- round(data2,3)
-    return(data2)
   }
+
+
+  if (method == "method") {
+
+
+    sparse_data1 <- expr_data %*% Matrix::Diagonal(x = 1 / Matrix::colSums(expr_data)) * 10000
+
+    sparse_data1[] <- round(log2(sparse_data1[] + 1),3)
+
+    return(sparse_data1)
+
+  }
+
 
   if (method == "smart-seq2") {
 
@@ -38,30 +47,33 @@ data_normalized <- function(expr_data,method,genome = "hg38"){
 
 
     colnames(gene_length) <- c("gene_name","Length")
-    expr_data$gene_name <- rownames(expr_data)
-    use_data <- dplyr::inner_join(gene_length,expr_data) %>% as.data.frame()
 
 
-    result_value <- use_data
-    for (i in 3:ncol(use_data)) {
-      result <- round((use_data[,i]*1000*1000000)/(use_data[,2]*sum((use_data[,i]*1000/use_data[,2]))),3)
+    use_gene_length <- gene_length[gene_length$gene_name %in% rownames(expr_data),]
+    gene_names <- use_gene_length$gene_name
+    selected_rows <- expr_data[gene_names, ]
 
-      result_value[,i] <- result
+
+    compute_result <- function(x) {
+      round((x * 1000 * 1000000) / (use_gene_length[, 2] * sum(x * 1000 / use_gene_length[, 2])), 3)
     }
 
 
-    data1 <- result_value %>% dplyr::select(-Length,-gene_name) %>% as.data.frame()
-    data2 <- data1 %>% dplyr::mutate_all(funs(log2(.+1)))
-    rownames(data2) <- use_data$gene_name
-    return(data2)
+    result_matrix <- as.data.frame(apply(selected_rows, 2, compute_result))
+    colnames(result_matrix) <- colnames(selected_rows)
+    rownames(result_matrix) <- rownames(selected_rows)
+
+    sparse_data1 <- Matrix::Matrix(as.matrix(result_matrix),sparse = T)
+    sparse_data1[] <- round(log2(sparse_data1[] + 1),3)
+
+
+    return(sparse_data1)
   }
 
 
-
-  if (!all(method %in% c("10X", "smart-seq2"))) {
-    stop("Please choose a sequencing method 10X or smart-seq2")
+  if (!all(method %in% c("method", "smart-seq2"))) {
+    stop("Method parameter error ")
   }
-
 }
 
 
